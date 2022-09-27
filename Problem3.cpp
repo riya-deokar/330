29bea7e6bad7a8247f92588e4743487abe829d1a
riya-deokar <47408944+riya-deokar@users.noreply.github.com>
2022-09-27 13:39:46 -0400

    Add files via upload

---
+++ Problem3.cpp
@@ -0,0 +1,40 @@
+#include "Problem3.h"
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rotateVec(string word){
+    vector<int> diff(word.length()); 
+    for(int i =0; i<word.size()-1; i++){
+        if(int(word[i+1])-int(word[i]) >= 0) {
+            diff[i] = int(word[i+1])-int(word[i]);
+        } 
+        else{
+            diff[i] = (int(word[i+1])-int(word[i]))+26;
+        }
+    }
+    return diff; 
+} 
+
+void findRotations(string list[], int length){
+    int flag[length];
+    vector<int> nothing(1,1);
+    vector<vector<int>> spaces(length,nothing); 
+    for(int i=0; i<length; i++){
+        spaces.at(i) = rotateVec(list[i]); 
+    }
+    for(int j=0; j<length; j++){
+        for(int k=j+1; k<length; k++){
+            if(spaces.at(j) == spaces.at(k)){
+                flag[k] = 1; 
+                flag[j] = 1;
+                
+            }
+        }
+    }
+    for(int i = 0; i < length; i++){
+        if(flag[i] == 1)
+            cout << list[i] << endl;
+    }
+}
---
+++ Problem3.h
@@ -0,0 +1,22 @@
+//
+//  Problem3.h
+//  rotation
+//
+//  Copyright Â© 2022 Tali Moreshet. All rights reserved.
+//
+
+#ifndef Problem3_h
+#define Problem3_h
+
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findRotations(string list[], int length);
+vector<int> rotateVec(string word);
+
+
+#endif /* Problem3_h */
+
+
